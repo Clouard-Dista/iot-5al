28c2b761d6e0f7496833e484d7196703fe7dfe03
NOIRET <noiret.remy@gmail.com>
2019-04-17 16:24:57 +0200

    Add JSON in /sensor routes with temperature, pression and humidity data

--- web_server/web_server.ino
+++ web_server/web_server.ino
@@ -1,15 +1,34 @@
+// Adafruit_DHT library
+#include <DHT.h>
+
+// Declaration input pin
+#define DHTPIN 5 //Temperature+Humidity//
+
+// Initialisation capteur
+#define DHTTYPE DHT11   // DHT 11
+#define Digital_polution A0 //polution
+DHT dht(DHTPIN, DHTTYPE);
+
+const int but_pin = D5;
+
+// WEB //
 #include <FS.h> // pour le SPIFFS
-const char * nomDeFichier = "/index.html";
+const char * nomDeFichier = "/sensor.html";
 // provient de https://github.com/esp8266/Arduino 
 // télécharger et installer à la main la dernière version
-#include <ESP8266WiFi.h>               
+#include <ESP8266WiFi.h>           
 
+const byte maxHTTPLine = 100;    
+char httpLine[maxHTTPLine + 1]; // +1 pour avoir la place du '\0'
 const char* ssid = "Sonde"; // <<--- METTRE ICI VOTRE NOM RESEAU WIFI
 const char* password = "12345678"; // <<--- METTRE ICI VOTRE MOT DE PASSE WIFI
 
 const uint16_t HTTPPort = 80;
-WiFiServer serveurWeb(HTTPPort); // crée un serveur sur le port HTTP standard
+const byte maxURL = 50;
+char urlRequest[maxURL + 1]; // +1 pour avoir la place du '\0'
 
+WiFiServer serveurWeb(HTTPPort); // crée un serveur sur le port HTTP standard
+// FIN WEB //
 void printHTTPServerInfo()
 {
   Serial.print(F("Site web http://"));
@@ -21,12 +40,25 @@ void printHTTPServerInfo()
   Serial.println();
 }
 
-void testRequeteWeb()
+
+void getStateSensor(WiFiClient &cl, float &h , float &t, float &p){
+  cl.print(h);
+  cl.print(";");
+  cl.print(t);
+  cl.print(";");
+  cl.println(p);
+}
+
+void testRequeteWeb(float &h ,float &t,float &p)
 {
   boolean currentLineIsBlank = true;
+  byte indexMessage = 0;
+  boolean requeteHTTPRecue = false;
+  
+  char * ptrGET, *ptrEspace;
 
   WiFiClient client = serveurWeb.available();
-  if (!client) return; // pas de client connecté
+  if (!client) return ; // pas de client connecté
 
   while (client.connected()) {
     if (client.available()) {
@@ -37,26 +69,58 @@ void testRequeteWeb()
         // ON GENERE LA PAGE WEB
         // On envoie un en tête de réponse HTTP standard
         client.println(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"));
-
-                if (SPIFFS.exists(nomDeFichier)) {
-          File pageWeb = SPIFFS.open(nomDeFichier, "r");
-          client.write(pageWeb);
-          pageWeb.close();
-        } else {
-          Serial.println(F("Erreur de fichier"));
-        }
+          if(strstr(urlRequest, "/getStateSensor")){
+            getStateSensor(client, h ,t, p);
+          }
+          else {
+            if(SPIFFS.exists(nomDeFichier)){              
+              File pageWeb = SPIFFS.open(nomDeFichier, "r");
+              client.write(pageWeb);
+              pageWeb.close();
+            }else {
+              Serial.println(F("Erreur de fichier"));
+            }
+          }
         // on sort du while et termine la requête
         break;
       }
-      if (c == '\n') currentLineIsBlank = true;
-      else if (c != '\r') currentLineIsBlank = false;
+      if (c == '\n') {
+        currentLineIsBlank = true;
+
+        httpLine[indexMessage] = '\0'; // on termine la ligne correctement (c-string)
+        indexMessage = 0; // on se reprépre pour la prochaine ligne
+        if (ptrGET = strstr(httpLine, "GET")) {
+          // c'est la requête GET, la ligne continent "GET /URL HTTP/1.1", on extrait l'URL
+          ptrEspace = strstr(ptrGET + 4, " ");
+          *ptrEspace = '\0';
+          strncpy(urlRequest, ptrGET + 4, maxURL);
+          urlRequest[maxURL] = '\0'; // par précaution si URL trop longue
+        }
+      }
+      else if (c != '\r') {
+        currentLineIsBlank = false;
+        if (indexMessage <= maxHTTPLine - 1) {
+          httpLine[indexMessage++] =  c; // sinon on ignore le reste de la ligne
+        }
+      }
     } // end if available
   } // end while
   delay(1);
   client.stop(); // termine la connexion
+
 }
 
 void setup() {
+
+  
+  pinMode(but_pin, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+ 
+  // demare les mesures
+  dht.begin();
+
+  // WEB //
   Serial.begin(74880); // parce que mon Wemos et par défaut à peu près à cette vitesse, évite les caractères bizarre au boot
   Serial.println("\n\nTest SPIFFS\n");
 
@@ -79,8 +143,51 @@ void setup() {
   serveurWeb.begin();
   printHTTPServerInfo();
 
+  // FIN WEB //
+
 }
 
 void loop() {
-  testRequeteWeb();
+
+  buttonToggleLed();
+  delay(100);
+  // if (digitalRead(LED_BUILTIN) != 1) {
+    //humidity
+    float h = dht.readHumidity();
+    //temperature
+    float t = dht.readTemperature();
+    //pollution//Digital_polution/10 > 75 = aie
+    float p = analogRead (Digital_polution)/10;
+     
+    // affichÃ© qu'il y a une erreur si une des valeur n'est pas prÃ©sente
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Error while reading the sensor");
+      return;
+    }
+
+    //Serial.print("Humidity(%): ");
+    Serial.print(h);
+    Serial.print(" ");
+    
+    //Serial.print("temperature(C): ");
+    Serial.print(t);  
+    Serial.print(" ");
+    
+    //Serial.print("polution: ");
+    Serial.println(p);
+  // }
+
+  // WEB //
+  testRequeteWeb(h, t, p);
+}
+
+void buttonToggleLed() {
+  static bool old_but_state = 0;
+  if (digitalRead(but_pin) == 0 && old_but_state == 1) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(200);
+  }
+
+  //Serial.println(digitalRead(but_pin));
+  old_but_state = digitalRead(but_pin);
 }
